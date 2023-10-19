2bfebff0aad4656e0a70b5c4e342ce077ab518b8
melmore <melmore@MARVINs-MBP-2.home>
2023-10-19 15:57:32 -0400

    move skaffold and update paths | setup docker files node and python

--- backend/python/notifications/app.py
+++ backend/python/notifications/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return 'Hello, Flask World!'
 
+
 if __name__ == '__app__':
-    app.run(host="0.0.0.0", port=5051)
+    app.run(host="0.0.0.0", port=5050)
