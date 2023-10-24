5fe28d6272cde0dd01bc10a8ddcb7127ddb74018
melmore <melmore@MARVINs-MBP-2.home>
2023-10-24 16:56:14 -0400

    write unit test for auth service

--- backend/python/test/selenium/test.py
+++ backend/python/test/selenium/test.py
@@ -12,7 +12,7 @@ driver = webdriver.Chrome()
 # Set up a route to interact with the button
 @app.route('/test_button')
 def test_button():
-    driver.get('http://127.0.0.1:5000')  # Your Flask app's URL
+    driver.get('http://127.0.0.1:5001')  # Your Flask app's URL
     button = driver.find_element_by_id('testButton')
     button.click()
     return 'Button clicked'
