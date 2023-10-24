d5e834b41eb87828a03a4fd9d623f83dbded25f7
melmore <melmore@MARVINs-MBP-2.home>
2023-10-24 15:05:52 -0400

    refactor current user logic and add middleware to check

---
+++ backend/python/test/selenium/test.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from flask import Flask, render_template
+
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Start a browser instance
+driver = webdriver.Chrome()
+
+
+# Set up a route to interact with the button
+@app.route('/test_button')
+def test_button():
+    driver.get('http://127.0.0.1:5000')  # Your Flask app's URL
+    button = driver.find_element_by_id('testButton')
+    button.click()
+    return 'Button clicked'
+
+
+if __name__ == '__test__':
+    app.run()
